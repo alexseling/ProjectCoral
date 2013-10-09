26940097eaaa22d23954a991397fa73b5c98104c
AnthonyKazyaka <ask.anthony@hotmail.com>
2013-10-08 20:59:43 -0400

    Set up the basics for the changing of the GameScreens

---
+++ ProjectCoral/ProjectCoral/GameScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Schema;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectCoral
+{
+    public class GameScreen
+    {
+
+        public ProjectCoralGame _game;
+        public ProjectCoralGame Game {get { return _game; }}
+
+        public GameScreen(ProjectCoralGame game)
+        {
+            this._game = game;
+        }
+
+        public virtual void Initialize() {}
+        public virtual void LoadContent() {}
+        public virtual void Activate() {}
+        public virtual void Deactivate() {}
+        public virtual void Update(GameTime gameTime) {}
+        public virtual void Draw(GameTime gameTime) {}
+        public virtual void DrawModel(GameTime gameTime, SpriteBatch spriteBatch) {}
+
+    }
+}
---
+++ ProjectCoral/ProjectCoral/MenuGameScreen.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectCoral
+{
+    public class MenuGameScreen : GameScreen
+    {
+
+        public MenuGameScreen(ProjectCoralGame game) : base(game)
+        {
+            this._game = game;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Activate()
+        {
+            base.Activate();
+        }
+
+        public override void Deactivate()
+        {
+            base.Deactivate();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        public override void DrawModel(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            base.DrawModel(gameTime, spriteBatch);
+        }
+    }
+}
